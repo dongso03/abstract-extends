4225af318da0943cbeac29c851d25031bcf53487
큰동 <@39.124.217.123>
2024-01-01 23:49:38 +0900

    불고기 에드토핑 메서드 수정

--- Pizza/src/BulgogiPizza.java
+++ Pizza/src/BulgogiPizza.java
@@ -5,7 +5,8 @@ public class BulgogiPizza extends Pizza {
 
     @Override
     public void addToppings() {
-        System.out.println("불고기 토핑 추가 중");
+    	System.out.println("불고기 토핑 추가 하시겠습니까? (추가 금액:2000원)");	
+        System.out.println("원하면 1번 아니면 다른거아무거나");
     }
 
     // cook 메서드 오버라이드
